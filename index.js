6c15f1b146ebb4bddd1efcfec9998658b9097a61
abrusu-collins <snillochemingson@gmail.com>
2022-11-09 18:15:10 +0000

    nice

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-$(function() {
-    $('.skitter-large').skitter();
-  });
+var navbar = document.querySelector('.nav')
 
+window.onscroll = function() {
+
+  // pageYOffset or scrollY
+  if (window.pageYOffset > 0 ) {
+    navbar.classList.add('scrolled')
+  } else {
+    navbar.classList.remove('scrolled')
+  }
+}
+
+document.querySelector('.toggler').onclick = () => {
+  document.querySelector('.nav').classList.toggle('open');
+}
